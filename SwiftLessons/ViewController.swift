ea84d1ca3b421325bb43ffdb3bc228d4d60afd56
kipdurron <ishunin97@gmail.com>
2020-05-13 20:42:11 +0300

    Initial Commit

---
+++ SwiftLessons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftLessons
+//
+//  Created by Илья Кадыров on 13.05.2020.
+//  Copyright © 2020 Илья Ишунин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
