8ad341f3c52e7cf54804820515fa6669a46dbc57
kipdurron <ishunin97@gmail.com>
2020-05-23 22:19:24 +0300

    init

---
+++ SwiftLessons/ClassWorks/cw_4.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class People {
+    
+    private var name:String = ""
+    var age:Int = 18
+    
+}
+
+class Student: People {
+    var kaf: Int = 22
+}
+let ilya: Student = Student()
+ilya.age = 3
+var = People
